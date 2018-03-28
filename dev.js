a8585b02b71caee3f9060edb8363b65cac07dcc1
Pawel Psztyc <jarrodek@gmail.com>
2018-03-28 09:39:29 -0700

    Updated headers model

--- dev.js
+++ dev.js
@@ -6,11 +6,11 @@ amf.plugins.document.Vocabularies.register();
 amf.plugins.features.AMFValidation.register();
 
 // const dataPath = 'raml-examples/raml-query-params';
-// const dataPath = 'raml-examples/raml-headers';
+const dataPath = 'raml-examples/raml-headers';
 // const dataPath = 'raml-examples/raml-body-params';
 // const dataPath = 'raml-examples/raml-auth';
 // const dataPath = 'raml-examples/url-params';
-const dataPath = 'raml-examples/raml-example-api';
+// const dataPath = 'raml-examples/raml-example-api';
 amf.Core.init().then(() => {
   const ramlParser = amf.Core.parser('RAML 1.0', 'application/yaml');
   const jsonLdParser = amf.Core.generator('AMF Graph', 'application/ld+json');
