caee8b368728e84710c4aab45d3c22575c98ec7b
Pawel Psztyc <jarrodek@gmail.com>
2018-04-16 14:57:39 -0700

    Update

--- dev.js
+++ dev.js
@@ -6,18 +6,19 @@ amf.plugins.document.Vocabularies.register();
 amf.plugins.features.AMFValidation.register();
 
 // const dataPath = 'raml-examples/raml-query-params';
-const dataPath = 'raml-examples/raml-headers';
+// const dataPath = 'raml-examples/raml-headers';
 // const dataPath = 'raml-examples/raml-body-params';
 // const dataPath = 'raml-examples/raml-auth';
 // const dataPath = 'raml-examples/url-params';
-// const dataPath = 'raml-examples/raml-example-api';
+const dataPath = 'raml-examples/raml-example-api';
+// const dataPath = 'raml-examples/exchange-experience-api';
 amf.Core.init().then(() => {
   const ramlParser = amf.Core.parser('RAML 1.0', 'application/yaml');
   const jsonLdParser = amf.Core.generator('AMF Graph', 'application/ld+json');
   ramlParser.parseFileAsync(`file://${dataPath}/api.raml`)
   .then((doc) => {
     const r = amf.Core.resolver('RAML 1.0');
-    doc = r.resolve(doc);
+    doc = r.resolve(doc, 'editing');
     return jsonLdParser.generateString(doc);
   })
   .then((data) => {
